08be1bc8f94c19b5f135475ad233c51ea00bc3e4
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-11-24 17:16:46 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC 
+
+
+clf = DecisionTreeClassifier()
+clf.fit()
+
+clf = GaussianNB()
+clf.fit()
